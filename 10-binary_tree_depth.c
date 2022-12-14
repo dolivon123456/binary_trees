af0fb30d6eb9769e3e5a349fcb2fc96f706e9e17
Olatunji Adedolapo Sharon <105457522+dolivon123456@users.noreply.github.com>
2022-12-14 13:21:56 +0100

    Update 10-binary_tree_depth.c

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -3,13 +3,13 @@
 /**
  * binary_tree_depth - measure the depth of a node in a binary tree.
  * Depth is measured from root parent node going down.
- * @node: pointer to node to measure depth of
+ * @tree: pointer to node to measure depth of
  * Return: depth of tree from given node
  */
-size_t binary_tree_depth(const binary_tree_t *node)
+size_t binary_tree_depth(const binary_tree_t *tree)
 {
-if (node == NULL || node->parent == NULL)
+if (tree == NULL || tree->parent == NULL)
 	return (0);
 
-return (binary_tree_depth(node->parent) + 1);
+return (binary_tree_depth(tree->parent) + 1);
 }
